28291c4d34a94a3b58901caf41a0b36bf1ffdad7
kurtisp <kurtis.pace2011@gmail.com>
2019-04-12 13:25:25 -0600

    eslint was configured for something then what we watned

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
 	"env": {
 		"browser": true,
+		"commonjs": true,
 		"es6": true
 	},
 	"extends": "eslint:recommended",
@@ -9,8 +10,7 @@ module.exports = {
 		"SharedArrayBuffer": "readonly"
 	},
 	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
+		"ecmaVersion": 2018
 	},
 	"rules": {
 		"indent": [
