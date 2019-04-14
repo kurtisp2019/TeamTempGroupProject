8ea0e1af415aa4185d687496bee74482f17c5993
PlatinumPi <goldpi101@gmail.com>
2019-04-14 12:13:02 -0600

    Express pathways

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-/**
- * 
- *      server.js
- * 
- */
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes")(app); // Routes need to be specified. One 'require' per route
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log(`App listening on PORT: ${PORT}`);
+    });
+});
