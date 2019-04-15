01ddea4e56a8b3d01128b42d42f844204be5bd16
kurtisp <kurtis.pace2011@gmail.com>
2019-04-15 11:23:38 -0600

    models-controllers

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 	"rules": {
 		"indent": [
 			"error",
-			"tab"
+			"space"
 		],
 		"linebreak-style": [
 			"error",
--- models/shopping-cart.js
+++ models/shopping-cart.js
@@ -7,18 +7,18 @@
 module.exports = function (_sequelize, _Sequelize) {
 
     var ShoppingCart = _sequelize.define("ShoppingCart", {
-        
+
         storeItemId: {
             type: _Sequelize.INTEGER,
             allowNull: true
         },
-        userId: {
+        CurUserId: {
             type: _Sequelize.INTEGER,
             allowNull: false
         }
     });
 
-    ShoppingCart.associate = function (_models) { 
+    ShoppingCart.associate = function (_models) {
 
         ShoppingCart.belongsTo(_models.User, {
             foreignKey: {
